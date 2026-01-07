e2019096b18ced272e2d5de787b6e2b69dcce16c
Nate <daviesnathanael45@gmail.com>
2026-01-07 22:25:44 +0100

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <div>
+          <h1>the text chat goes here</h1>
+        </div>
+        <div>
+          <h1 className='input-box'>the input box goes here</h1>
+        </div>
+      </div>
+  )
+}
+
+export default App
