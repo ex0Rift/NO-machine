e37bf94d6fab0ee165c9e3597e415b5093bd3365
Nate <daviesnathanael45@gmail.com>
2026-01-08 09:59:36 +0100

    added temporrary list for chat messages, will be expanded on

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,28 @@ import './App.css'
 function App() {
   const [value, setValue] = useState("");
 
+  const chat = [
+    {text:'blah blah blah', user:true, id:1},
+    {text: 'i said some stuff', user:false, id:2}
+  ];
+
+  const listChat = chat.map(message =>
+    <li
+      key={message.id}
+      style = {{
+        color: message.user ? 'green' : 'blue'
+      }}
+    >
+      {message.text}
+    </li>
+  );
+          
+
   return (
       <div>
         <div>
           <h1>the text chat goes here {value}</h1>
+          <ul>{listChat}</ul>
         </div>
         <div className='input-box'>
           <input
@@ -15,6 +33,7 @@ function App() {
             value={value}
             onChange={e => setValue(e.target.value)}
           />
+          <button>→</button>
         </div>
       </div>
   )
