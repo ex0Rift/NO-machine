a35d75628ee687445eb7bb545167181f0d9529b1
Nate <daviesnathanael45@gmail.com>
2026-01-13 13:13:51 +0100

    made it so the text scrolls isntead of does a weird clipping with bottom box when there is too much stuff in the text list to display on the screen. stopped the user from being able to send blank messages. AND added comments to all my code so i can come back to thiis later and ot be confused.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
-
 
 function App() {
   type chat = {text: string; user: boolean; id: number;};
 
   const [value, setValue] = useState("");
-
   const [chat, setChat] = useState<chat[]>([
 
   ]);
 
+  //creates the map for the chat list for displaying messages on the screen
   const listChat = chat.map(message =>
     <li
       key={message.id}
       style = {{
+        //colour depending on which user
         color: message.user ? '#ffffff' : '#b6b6b6ff'
       }}
     > 
@@ -24,16 +23,29 @@ function App() {
     </li>
   );
 
+  //this component makes the text scroll in the div when there is too much to display in the screen
+  useEffect(() => {
+    const container = document.querySelector('.chat-area');
+    if (container) container.scrollTop = container.scrollHeight;
+  }, [chat]);
+
+//this function happens every time a message is sent
 function SendMessage(input:string){
+  if (input === "")return;//if empty does nothing
+
+  //adds the useres text to the list
   const newItem = {text:input,user:true,id:Object.keys(chat).length+1};
   setChat([...chat,newItem]);
   
+  //fetch from the NO-AS-A-SERVICE API for a response
   fetch("https://naas.isalman.dev/no")
     .then(r => r.json())
     .then(response => {
+      //adds response to the chat list
       const newItem_bot = {text:response.reason,user:false,id:Object.keys(chat).length+2};
       setChat(prevChat => [...prevChat,newItem_bot]);
     });
+  //empites the user input box
   setValue("");
 }
 
